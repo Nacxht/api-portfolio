d7813813b0585c9c13dc9ef5804b3ad39d14f308
Nacxht <zombiecrafter069@gmail.com>
2025-05-11 18:14:15 +0700

    chore: removed unused file

--- src/api/routers/project_screenshot.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from "@hono/hono";
-
-export const projectScreenshotRouter = new Hono();
-
-projectScreenshotRouter.get("/");
-projectScreenshotRouter.post("/");
-projectScreenshotRouter.patch("/");
-projectScreenshotRouter.delete("/");
